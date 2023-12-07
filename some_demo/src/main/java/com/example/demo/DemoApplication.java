a092dea8b4b7b1d727c7601a8bf790952502f049
Aleksandr <device.ai@gmail.com>
2023-12-07 15:07:08 +0200

    User properties

--- some_demo/src/main/java/com/example/demo/DemoApplication.java
+++ some_demo/src/main/java/com/example/demo/DemoApplication.java
@@ -4,14 +4,19 @@ import com.example.demo.bussineslayer.HelloEvent;
 import com.example.demo.bussineslayer.PojoEvent;
 import com.example.demo.bussineslayer.mycar.Car;
 import com.example.demo.bussineslayer.mycar.Config;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@PropertySource("classpath:user.properties")
 public class DemoApplication {
 
 	public static void main(String[] args) {
@@ -23,6 +28,8 @@ public class DemoApplication {
 @Component
 class Runner implements CommandLineRunner {
 	private final ApplicationEventPublisher eventPublisher;
+	@Autowired
+	private Environment env;
 
 	public Runner(ApplicationEventPublisher eventPublisher) {
 		this.eventPublisher = eventPublisher;
@@ -42,6 +49,9 @@ class Runner implements CommandLineRunner {
 		Car myCar = context.getBean("tesla", Car.class);
 		System.out.println(myCar.getName());
 		System.out.println(myCar.getEngine().getBrand());
+
+		System.out.println("KEY is: " + env.getProperty("MY_KEY"));
+		System.out.println("USER NAME is: " + env.getProperty("USER_NAME"));
 
 		System.out.println("The application was ended");
 	}
